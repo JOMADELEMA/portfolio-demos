f3572ad1ca687c06296cd38771b0d747700c41ff
JOMA-OFFICE <jomadelema@hotmail.com>
2022-08-26 16:12:30 -0600

    se continua la ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,128 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <>HomePage</>;
+  return (
+    <>
+      <div className="flex flex-col items-center w-full">
+        <div className="flex justify-center my-5">
+          <Link to="/html">
+            <div className="shadow hover:shadow-md rounded-full mx-2 px-3 py-2 bg-orange-400 dark:bg-orange-600">
+              HTML
+            </div>
+          </Link>
+          <Link to="/react">
+            <div className="shadow hover:shadow-md rounded-full mx-2 px-3 py-2 bg-blue-400 dark:bg-blue-600">
+              React JS
+            </div>
+          </Link>
+          <Link to="/javascript">
+            <div className="shadow hover:shadow-md rounded-full mx-2 px-3 py-2 bg-yellow-400 dark:bg-yellow-600">
+              Javascript
+            </div>
+          </Link>
+        </div>
+        <div className="h-96 border w-10/12 rounded shadow my-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          aliquam, nisi totam fugiat quaerat possimus! Neque, distinctio fugiat
+          officiis vero tenetur maxime eius tempora quas error est vel. Minima
+          nam tempora sint consectetur sed, voluptatum dicta perspiciatis
+          ducimus. Enim quos reprehenderit, explicabo ullam blanditiis dolorem
+          in animi quisquam modi quo excepturi quidem eligendi ratione aut
+          deleniti consequatur veniam tempora. At mollitia vitae iusto fuga
+          inventore commodi laborum saepe obcaecati voluptatum ratione
+          architecto quaerat porro sequi quos, laboriosam a, voluptas odit
+          voluptate quidem impedit aliquid quia. Sit, dolore dolores ullam nulla
+          aspernatur delectus ea? Exercitationem, omnis dolores reiciendis
+          accusantium repudiandae incidunt.
+        </div>
+        <div className="h-96 border w-10/12 rounded shadow my-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          aliquam, nisi totam fugiat quaerat possimus! Neque, distinctio fugiat
+          officiis vero tenetur maxime eius tempora quas error est vel. Minima
+          nam tempora sint consectetur sed, voluptatum dicta perspiciatis
+          ducimus. Enim quos reprehenderit, explicabo ullam blanditiis dolorem
+          in animi quisquam modi quo excepturi quidem eligendi ratione aut
+          deleniti consequatur veniam tempora. At mollitia vitae iusto fuga
+          inventore commodi laborum saepe obcaecati voluptatum ratione
+          architecto quaerat porro sequi quos, laboriosam a, voluptas odit
+          voluptate quidem impedit aliquid quia. Sit, dolore dolores ullam nulla
+          aspernatur delectus ea? Exercitationem, omnis dolores reiciendis
+          accusantium repudiandae incidunt.
+        </div>
+        <div className="h-96 border w-10/12 rounded shadow my-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          aliquam, nisi totam fugiat quaerat possimus! Neque, distinctio fugiat
+          officiis vero tenetur maxime eius tempora quas error est vel. Minima
+          nam tempora sint consectetur sed, voluptatum dicta perspiciatis
+          ducimus. Enim quos reprehenderit, explicabo ullam blanditiis dolorem
+          in animi quisquam modi quo excepturi quidem eligendi ratione aut
+          deleniti consequatur veniam tempora. At mollitia vitae iusto fuga
+          inventore commodi laborum saepe obcaecati voluptatum ratione
+          architecto quaerat porro sequi quos, laboriosam a, voluptas odit
+          voluptate quidem impedit aliquid quia. Sit, dolore dolores ullam nulla
+          aspernatur delectus ea? Exercitationem, omnis dolores reiciendis
+          accusantium repudiandae incidunt.
+        </div>
+        <div className="h-96 border w-10/12 rounded shadow my-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          aliquam, nisi totam fugiat quaerat possimus! Neque, distinctio fugiat
+          officiis vero tenetur maxime eius tempora quas error est vel. Minima
+          nam tempora sint consectetur sed, voluptatum dicta perspiciatis
+          ducimus. Enim quos reprehenderit, explicabo ullam blanditiis dolorem
+          in animi quisquam modi quo excepturi quidem eligendi ratione aut
+          deleniti consequatur veniam tempora. At mollitia vitae iusto fuga
+          inventore commodi laborum saepe obcaecati voluptatum ratione
+          architecto quaerat porro sequi quos, laboriosam a, voluptas odit
+          voluptate quidem impedit aliquid quia. Sit, dolore dolores ullam nulla
+          aspernatur delectus ea? Exercitationem, omnis dolores reiciendis
+          accusantium repudiandae incidunt.
+        </div>
+        <div className="h-96 border w-10/12 rounded shadow my-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          aliquam, nisi totam fugiat quaerat possimus! Neque, distinctio fugiat
+          officiis vero tenetur maxime eius tempora quas error est vel. Minima
+          nam tempora sint consectetur sed, voluptatum dicta perspiciatis
+          ducimus. Enim quos reprehenderit, explicabo ullam blanditiis dolorem
+          in animi quisquam modi quo excepturi quidem eligendi ratione aut
+          deleniti consequatur veniam tempora. At mollitia vitae iusto fuga
+          inventore commodi laborum saepe obcaecati voluptatum ratione
+          architecto quaerat porro sequi quos, laboriosam a, voluptas odit
+          voluptate quidem impedit aliquid quia. Sit, dolore dolores ullam nulla
+          aspernatur delectus ea? Exercitationem, omnis dolores reiciendis
+          accusantium repudiandae incidunt.
+        </div>
+        <div className="h-96 border w-10/12 rounded shadow my-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          aliquam, nisi totam fugiat quaerat possimus! Neque, distinctio fugiat
+          officiis vero tenetur maxime eius tempora quas error est vel. Minima
+          nam tempora sint consectetur sed, voluptatum dicta perspiciatis
+          ducimus. Enim quos reprehenderit, explicabo ullam blanditiis dolorem
+          in animi quisquam modi quo excepturi quidem eligendi ratione aut
+          deleniti consequatur veniam tempora. At mollitia vitae iusto fuga
+          inventore commodi laborum saepe obcaecati voluptatum ratione
+          architecto quaerat porro sequi quos, laboriosam a, voluptas odit
+          voluptate quidem impedit aliquid quia. Sit, dolore dolores ullam nulla
+          aspernatur delectus ea? Exercitationem, omnis dolores reiciendis
+          accusantium repudiandae incidunt.
+        </div>
+        <div className="h-96 border w-10/12 rounded shadow my-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          aliquam, nisi totam fugiat quaerat possimus! Neque, distinctio fugiat
+          officiis vero tenetur maxime eius tempora quas error est vel. Minima
+          nam tempora sint consectetur sed, voluptatum dicta perspiciatis
+          ducimus. Enim quos reprehenderit, explicabo ullam blanditiis dolorem
+          in animi quisquam modi quo excepturi quidem eligendi ratione aut
+          deleniti consequatur veniam tempora. At mollitia vitae iusto fuga
+          inventore commodi laborum saepe obcaecati voluptatum ratione
+          architecto quaerat porro sequi quos, laboriosam a, voluptas odit
+          voluptate quidem impedit aliquid quia. Sit, dolore dolores ullam nulla
+          aspernatur delectus ea? Exercitationem, omnis dolores reiciendis
+          accusantium repudiandae incidunt.
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
